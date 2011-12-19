a272f65d1ee651f46799075c765dec91b2323d3d
dozedoff <dozedoffagain@gmail.com>
2011-12-19 23:33:15 +0100

    finished Board.java test

--- test/board/BoardTest.java
+++ test/board/BoardTest.java
@@ -1,22 +1,47 @@
 package board;
 
 import static org.junit.Assert.*;
+import static org.junit.matchers.JUnitMatchers.containsString;
+import static org.hamcrest.CoreMatchers.*;
+import static org.mockito.Mockito.*;
 
-import org.junit.Ignore;
+import java.util.LinkedList;
+
+import org.junit.Before;
 import org.junit.Test;
 
+import thread.WorkQueue;
+
 public class BoardTest {
-
+	Board board;
+	
+	LinkedList<Page> pages;
+	WorkQueue pageQueue;
+	Page page;
+	
+	
+	@Before
+	public void setup(){
+		pages = new LinkedList<>();
+		pageQueue = mock(WorkQueue.class);
+		page = mock(Page.class);
+		
+		pages.add(page);
+		
+		board = new Board(pages, pageQueue, "t");
+	}
+	
 	@Test
-	@Ignore
 	public void testToString() {
-		fail("Not yet implemented");
+		assertThat(board.toString(), is("/t/"));
 	}
 
 	@Test
-	@Ignore
 	public void testGetStatus() {
-		fail("Not yet implemented");
+		assertThat(board.getStatus(), is("/t/  idle"));
+		board.start();
+		assertThat(board.getStatus(), containsString("running"));
+		board.stop();
+		assertThat(board.getStatus(), is("/t/  idle"));
 	}
-
 }
