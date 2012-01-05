0009b7ab225d011f617477d60cf2e67936934d89
dozedoff <dozedoffagain@gmail.com>
2012-01-05 17:11:23 +0100

    added test for main class

--- test/AllTests.java
+++ test/AllTests.java
@@ -2,9 +2,12 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import app.MainTest;
+
 
 @RunWith(Suite.class)
-@SuiteClasses({ 	Board.class, 
+@SuiteClasses({ 	MainTest.class,
+					Board.class, 
 					Filter.class, 
 					Io.class
 })
