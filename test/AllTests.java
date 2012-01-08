4c713ef1fbcd6b5ed1be9d90fab67767bfc2c42d
dozedoff <dozedoffagain@gmail.com>
2012-01-08 23:19:57 +0100

    Included app package in full test run

--- test/AllTests.java
+++ test/AllTests.java
@@ -2,11 +2,9 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import app.SettingValidatorTest;
-
 
 @RunWith(Suite.class)
-@SuiteClasses({ 	SettingValidatorTest.class,
+@SuiteClasses({ 	App.class,
 					Board.class, 
 					Filter.class, 
 					Io.class
