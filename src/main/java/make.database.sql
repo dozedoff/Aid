c09677316961a035dce6991fbd7064980143de42
dozedoff <dozedoffagain@gmail.com>
2012-12-25 11:48:55 +0100

    Made DB create script compatible with postgreSQL
    - Changed loaded JDBC in pom.xml

--- src/main/java/make.database.sql
+++ src/main/java/make.database.sql
@@ -1,4 +1,4 @@
--- --------------------------------------------------------
+﻿-- --------------------------------------------------------
 -- Host:                         localhost
 -- Server version:               5.5.24 - MySQL Community Server (GPL)
 -- Server OS:                    Win64
@@ -6,290 +6,226 @@
 -- Date/time:                    2012-08-08 17:48:51
 -- --------------------------------------------------------
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-SET NAMES utf8;
-SET FOREIGN_KEY_CHECKS=0;
-
 -- Dumping database structure for aid
-CREATE DATABASE IF NOT EXISTS `aid` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
-USE `aid`;
+--CREATE DATABASE "aid" WITH OWNER = aid ENCODING = 'UTF8';
+CREATE SCHEMA "aid" AUTHORIZATION aid;			
 
+SET "search_path" TO "aid";
+
+CREATE OR REPLACE FUNCTION update_changetimestamp_column()
+RETURNS TRIGGER AS $$
+BEGIN
+   NEW.timestamp = now(); 
+   RETURN NEW;
+END;
+$$ language 'plpgsql';
 
 -- Dumping structure for table aid.block
-CREATE TABLE IF NOT EXISTS `block` (
-  `id` varchar(64) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `hash` (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=ascii COMMENT='Blocked Items. Programm will tag files.';
+CREATE TABLE IF NOT EXISTS "block" (
+  "id" varchar(64) PRIMARY KEY UNIQUE NOT NULL
+);
+COMMENT ON TABLE "block" IS 'Blocked Items. Programm will tag files.';
 
 -- Data exporting was unselected.
 
 
 -- Dumping structure for table aid.cache
-CREATE TABLE IF NOT EXISTS `cache` (
-  `id` varchar(48) NOT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Recently downloaded files';
+CREATE TABLE IF NOT EXISTS "cache" (
+  "id" varchar(48) PRIMARY KEY UNIQUE NOT NULL,
+  "timestamp" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+COMMENT ON TABLE "cache" IS 'Recently downloaded files';
 
+CREATE TRIGGER update_cache_changetimestamp BEFORE UPDATE
+    ON "cache" FOR EACH ROW EXECUTE PROCEDURE 
+    update_changetimestamp_column();
 -- Data exporting was unselected.
 
 
 -- Dumping structure for table aid.dirlist
-CREATE TABLE IF NOT EXISTS `dirlist` (
-  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `dirpath` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `dirpath` (`dirpath`),
-  UNIQUE KEY `dirpath_unique` (`dirpath`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='List of all known directories';
+CREATE TABLE IF NOT EXISTS "dirlist" (
+  "id" serial NOT NULL PRIMARY KEY,
+  "dirpath" varchar(255) UNIQUE NOT NULL
+);
+CREATE INDEX "dirpath" ON "dirlist" ("dirpath");
+COMMENT ON TABLE "dirlist" IS 'List of all known directories';
 
 -- Data exporting was unselected.
 
 
 -- Dumping structure for table aid.dnw
-CREATE TABLE IF NOT EXISTS `dnw` (
-  `id` varchar(64) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `hash` (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=ascii COMMENT='Unwanted files';
+CREATE TABLE IF NOT EXISTS "dnw" (
+  "id" varchar(64) PRIMARY KEY UNIQUE NOT NULL
+);
+COMMENT ON TABLE "dnw" IS 'Unwanted files';
 
 -- Data exporting was unselected.
 
 
 -- Dumping structure for view aid.dupeview
 -- Creating temporary table to overcome VIEW dependency errors
-CREATE TABLE `dupeview` (
-	`id` VARCHAR(64) NOT NULL COLLATE 'ascii_general_ci',
-	`dupeloc` VARCHAR(30) NOT NULL COLLATE 'utf8_unicode_ci',
-	`dupePath` VARCHAR(510) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
-	`origloc` VARCHAR(30) NOT NULL COLLATE 'utf8_unicode_ci',
-	`origPath` VARCHAR(510) NULL DEFAULT NULL COLLATE 'utf8_general_ci'
-) ENGINE=MyISAM;
+CREATE TABLE "dupeview" (
+	"id" VARCHAR(64) NOT NULL,
+	"dupeloc" VARCHAR(30) NOT NULL,
+	"dupePath" VARCHAR(510) NULL DEFAULT NULL,
+	"origloc" VARCHAR(30) NOT NULL,
+	"origPath" VARCHAR(510) NULL DEFAULT NULL
+);
+
+-- Dumping structure for table aid.filelist
+CREATE TABLE IF NOT EXISTS "filelist" (
+  "id" serial PRIMARY KEY NOT NULL,
+  "filename" varchar(255) UNIQUE NOT NULL
+);
+COMMENT ON TABLE "filelist" IS 'List of all known filenames';
+
+CREATE INDEX "filename" ON "filelist" ("filename");
+
+-- Dumping structure for table aid.location_tags
+CREATE TABLE IF NOT EXISTS "location_tags" (
+  "tag_id" serial PRIMARY KEY UNIQUE NOT NULL,
+  "location" varchar(30) UNIQUE NOT NULL
+);
+COMMENT ON TABLE "location_tags" IS 'List of tags for different locations where data is stored';
+
+INSERT INTO "location_tags" ("tag_id", "location") VALUES (1, 'UNKNOWN');
+INSERT INTO "location_tags" ("tag_id", "location") VALUES (2, 'ARCHIVE');
 
 
+-- Data exporting was unselected.
 -- Dumping structure for table aid.fileduplicate
-CREATE TABLE IF NOT EXISTS `fileduplicate` (
-  `id` varchar(64) CHARACTER SET ascii NOT NULL,
-  `size` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `dir` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `filename` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `location` smallint(5) unsigned NOT NULL,
-  PRIMARY KEY (`id`,`dir`,`filename`),
-  UNIQUE KEY `id_dir_filename` (`id`,`dir`,`filename`),
-  KEY `location_FK` (`location`),
-  KEY `dir_FK` (`dir`),
-  KEY `file_fk` (`filename`),
-  CONSTRAINT `dup_dir_FK` FOREIGN KEY (`dir`) REFERENCES `dirlist` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `dup_file_fk` FOREIGN KEY (`filename`) REFERENCES `filelist` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `dup_location_FK` FOREIGN KEY (`location`) REFERENCES `location_tags` (`tag_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='List of duplicate files';
+CREATE TABLE IF NOT EXISTS "fileduplicate" (
+  "id" varchar(64)  NOT NULL,
+  "size" int NOT NULL DEFAULT '0',
+  "dir" int NOT NULL DEFAULT '0',
+  "filename" int NOT NULL DEFAULT '0',
+  "location" int NOT NULL,
+  PRIMARY KEY ("id","dir","filename"),
+  UNIQUE ("id","dir","filename"),
+  FOREIGN KEY ("dir") REFERENCES "dirlist" ("id") ON DELETE CASCADE,
+  FOREIGN KEY ("filename") REFERENCES "filelist" ("id") ON DELETE CASCADE,
+  FOREIGN KEY ("location") REFERENCES "location_tags" ("tag_id")
+);
+COMMENT ON TABLE "fileduplicate" IS 'List of duplicate files';
 
 -- Data exporting was unselected.
 
 
 -- Dumping structure for table aid.fileindex
-CREATE TABLE IF NOT EXISTS `fileindex` (
-  `id` varchar(64) CHARACTER SET ascii NOT NULL,
-  `size` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `dir` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `filename` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `location` smallint(5) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `hash` (`id`),
-  KEY `index_dir_FK` (`dir`),
-  KEY `index_file_fk` (`filename`),
-  KEY `index_location_FK` (`location`),
-  CONSTRAINT `index_dir_FK` FOREIGN KEY (`dir`) REFERENCES `dirlist` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `index_file_fk` FOREIGN KEY (`filename`) REFERENCES `filelist` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `index_location_FK` FOREIGN KEY (`location`) REFERENCES `location_tags` (`tag_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='all known files';
-
--- Data exporting was unselected.
-
-
--- Dumping structure for table aid.filelist
-CREATE TABLE IF NOT EXISTS `filelist` (
-  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `filename` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `filename` (`filename`),
-  UNIQUE KEY `filename_unique` (`filename`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='List of all known filenames';
-
--- Data exporting was unselected.
-
+CREATE TABLE IF NOT EXISTS "fileindex" (
+  "id" varchar(64)  PRIMARY KEY UNIQUE  NOT NULL,
+  "size" int NOT NULL DEFAULT '0',
+  "dir" int NOT NULL DEFAULT '0',
+  "filename" int NOT NULL DEFAULT '0',
+  "location" int NOT NULL,
+  FOREIGN KEY ("dir") REFERENCES "dirlist" ("id") ON DELETE CASCADE,
+  FOREIGN KEY ("filename") REFERENCES "filelist" ("id") ON DELETE CASCADE,
+  FOREIGN KEY ("location") REFERENCES "location_tags" ("tag_id")
+);
+COMMENT ON TABLE "fileindex" IS 'all known files';
 
 -- Dumping structure for table aid.filter
-CREATE TABLE IF NOT EXISTS `filter` (
-  `id` varchar(50) NOT NULL COMMENT 'full url',
-  `board` varchar(2) NOT NULL COMMENT 'board',
-  `reason` varchar(30) NOT NULL COMMENT 'filter description',
-  `status` tinyint(3) unsigned NOT NULL COMMENT 'filter status',
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='filtered threads';
+CREATE TABLE IF NOT EXISTS "filter" (
+  "id" varchar(50) PRIMARY KEY UNIQUE NOT NULL,
+  "board" varchar(2) NOT NULL,
+  "reason" varchar(30) NOT NULL,
+  "status" int NOT NULL,
+  "timestamp" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+COMMENT ON TABLE "filter" IS 'filtered threads';
 
+CREATE TRIGGER update_filter_changetimestamp BEFORE UPDATE
+    ON "filter" FOR EACH ROW EXECUTE PROCEDURE 
+    update_changetimestamp_column();
 -- Data exporting was unselected.
 
 
 -- Dumping structure for view aid.indexview
 -- Creating temporary table to overcome VIEW dependency errors
-CREATE TABLE `indexview` (
-	`id` VARCHAR(64) NOT NULL COLLATE 'ascii_general_ci',
-	`fullpath` VARCHAR(510) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
-	`size` BIGINT(20) UNSIGNED NOT NULL DEFAULT '0',
-	`location` VARCHAR(30) NOT NULL COLLATE 'utf8_unicode_ci'
-) ENGINE=MyISAM;
+CREATE TABLE "indexview" (
+	"id" VARCHAR(64) NOT NULL,
+	"fullpath" VARCHAR(510) NULL DEFAULT NULL,
+	"size" int NOT NULL DEFAULT '0',
+	"location" VARCHAR(30) NOT NULL
+);
 
+CREATE OR REPLACE FUNCTION list_compare()
+returns setof record as
 
--- Dumping structure for procedure aid.ListCompare
-DELIMITER //
-CREATE DEFINER=`root`@`localhost` PROCEDURE `ListCompare`()
-    READS SQL DATA
-    SQL SECURITY INVOKER
-    COMMENT 'Compare the index with block and dnw tables'
-BEGIN
+$$
 select indexview.id, indexview.fullpath, indexview.location from indexview join dnw on indexview.id=dnw.id;
 select indexview.id, indexview.fullpath, indexview.location from indexview join block on indexview.id=block.id;
-END//
-DELIMITER ;
+$$
+
+LANGUAGE SQL;
 
 
--- Dumping structure for table aid.location_tags
-CREATE TABLE IF NOT EXISTS `location_tags` (
-  `tag_id` smallint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `location` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`tag_id`),
-  UNIQUE KEY `tag_id` (`tag_id`),
-  UNIQUE KEY `location` (`location`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='List of tags for different locations where data is stored';
-
-INSERT IGNORE INTO `location_tags` (`tag_id`, `location`) VALUES (1, 'UNKNOWN');
-INSERT IGNORE INTO `location_tags` (`tag_id`, `location`) VALUES (2, 'ARCHIVE');
 
 -- Dumping structure for table aid.settings
-CREATE TABLE IF NOT EXISTS `settings` (
-  `name` varchar(20) NOT NULL,
-  `param` text NOT NULL,
-  UNIQUE KEY `name` (`name`)
-) ENGINE=MyISAM DEFAULT CHARSET=ascii COMMENT='Global settings for all clients';
+CREATE TABLE IF NOT EXISTS "settings" (
+  "name" varchar(20) UNIQUE NOT NULL,
+  "param" text NOT NULL
+);
+COMMENT ON TABLE "settings" IS 'Global settings for all clients';
 
-INSERT IGNORE INTO `settings` (`name`, `param`) VALUES ('SchemaVersion', '3');
-
-
-
--- Dumping structure for procedure aid.StripDriveLetter
-DELIMITER //
-CREATE DEFINER=`root`@`localhost` PROCEDURE `StripDriveLetter`()
-    MODIFIES SQL DATA
-    SQL SECURITY INVOKER
-BEGIN
-DECLARE done INT DEFAULT 0;
-  DECLARE i MEDIUMINT DEFAULT 0; 
-  DECLARE d VARCHAR(255) character set utf8;
-  DECLARE cur1 CURSOR FOR SELECT dirlist.id, SUBSTR(dirlist.dirpath FROM 3 FOR (CHAR_LENGTH(dirlist.dirpath))) FROM dirlist WHERE dirpath LIKE '_:%';
-
-  DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
-  SET unique_checks=0;
-  SET foreign_key_checks=0;
-
-  START TRANSACTION;
-  OPEN cur1;
-
-  read_loop: LOOP
-    FETCH cur1 INTO i,d;
-
-    IF done THEN
-      LEAVE read_loop;
-    END IF;
-    
-    UPDATE dirlist SET dirlist.dirpath = d WHERE dirlist.id = i;
-  END LOOP;
-
-  CLOSE cur1;
-  COMMIT;
-  
-  SET unique_checks=1;
-  SET foreign_key_checks=1;
-END//
-DELIMITER ;
-
+INSERT INTO "settings" ("name", "param") VALUES ('SchemaVersion', '3');
 
 -- Dumping structure for table aid.thumbs
-CREATE TABLE IF NOT EXISTS `thumbs` (
-  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `url` varchar(50) CHARACTER SET ascii NOT NULL,
-  `filename` varchar(25) CHARACTER SET ascii NOT NULL,
-  `thumb` blob NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Thumbnails for items in the Filter list';
+CREATE TABLE IF NOT EXISTS "thumbs" (
+  "id" serial PRIMARY KEY NOT NULL,
+  "url" varchar(50)  NOT NULL,
+  "filename" varchar(25)  NOT NULL,
+  "thumb" bytea NOT NULL
+);
+COMMENT ON TABLE "thumbs" IS 'Thumbnails for items in the Filter list';
 
 -- Data exporting was unselected.
 
-
--- Dumping structure for procedure aid.WarmUpDB
-DELIMITER //
-CREATE DEFINER=`root`@`localhost` PROCEDURE `WarmUpDB`()
-    READS SQL DATA
-    SQL SECURITY INVOKER
-    COMMENT 'Load all data in to memory (make sure there is enough memory available)'
-BEGIN
-	SELECT * FROM block;
-	SELECT * FROM  `cache`;
-	SELECT * FROM dirlist;
-	SELECT * FROM dnw;
-	SELECT * FROM fileduplicate;
-	SELECT * FROM fileindex;
-	SELECT * FROM filelist;
-	SELECT * FROM filter;
-	SELECT * FROM location_tags;
-	SELECT * FROM settings;
-	SELECT * FROM thumbs;
-END//
-DELIMITER ;
-
-
 -- Dumping structure for trigger aid.prune_thumbs_del
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
-DELIMITER //
-CREATE TRIGGER `prune_thumbs_del` AFTER DELETE ON `filter` FOR EACH ROW BEGIN
-	DELETE FROM thumbs WHERE url = OLD.id;
-END//
-DELIMITER ;
-SET SQL_MODE=@OLD_SQL_MODE;
+CREATE OR REPLACE FUNCTION prune_thumbs_del()
+RETURNS TRIGGER AS $$
+BEGIN
+   DELETE FROM thumbs WHERE url = OLD.id;
+END;
+$$ language PLPGSQL;
 
 
+CREATE TRIGGER prune_thumbs_del AFTER DELETE
+    ON "filter" FOR EACH ROW EXECUTE PROCEDURE 
+    prune_thumbs_del();
+	
 -- Dumping structure for trigger aid.prune_thumbs_up
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
-DELIMITER //
-CREATE TRIGGER `prune_thumbs_up` AFTER UPDATE ON `filter` FOR EACH ROW BEGIN
+
+CREATE OR REPLACE FUNCTION prune_thumbs_up()
+RETURNS TRIGGER AS $$
+BEGIN
 	IF NEW.status != 1 THEN
 		DELETE FROM thumbs WHERE url = OLD.id;
 	END IF;
-END//
-DELIMITER ;
-SET SQL_MODE=@OLD_SQL_MODE;
+END;
+$$ language PLPGSQL;
 
-
+CREATE TRIGGER prune_thumbs_up AFTER UPDATE
+    ON "filter" FOR EACH ROW EXECUTE PROCEDURE 
+    prune_thumbs_up();
+	
 -- Dumping structure for view aid.dupeview
 -- Removing temporary table and create final VIEW structure
-DROP TABLE IF EXISTS `dupeview`;
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` VIEW `dupeview` AS SELECT `fileduplicate`.id, dupeloc.location AS dupeloc, CONCAT(dirlist.dirpath, filelist.filename) AS dupePath, origloc.location AS origloc, CONCAT(idir.dirpath, ifile.filename) AS origPath FROM 
-	`fileduplicate` JOIN `fileindex` ON `fileduplicate`.id = `fileindex`.id 
-		JOIN filelist ON `fileduplicate`.filename=filelist.id
-			JOIN dirlist ON `fileduplicate`.dir=dirlist.id
+DROP TABLE IF EXISTS "dupeview";
+CREATE OR REPLACE VIEW "dupeview" AS 
+SELECT "fileduplicate".id, dupeloc.location AS dupeloc, CONCAT(dirlist.dirpath, filelist.filename) AS dupePath, origloc.location AS origloc, CONCAT(idir.dirpath, ifile.filename) AS origPath FROM 
+	"fileduplicate" JOIN "fileindex" ON "fileduplicate".id = "fileindex".id 
+		JOIN filelist ON "fileduplicate".filename=filelist.id
+			JOIN dirlist ON "fileduplicate".dir=dirlist.id
 				JOIN location_tags AS origloc ON origloc.tag_id=fileindex.location
-					JOIN dirlist AS idir ON idir.id = `fileindex`.dir 
-						JOIN filelist AS ifile ON `fileindex`.filename = ifile.id
+					JOIN dirlist AS idir ON idir.id = "fileindex".dir 
+						JOIN filelist AS ifile ON "fileindex".filename = ifile.id
 							JOIN location_tags AS dupeloc ON dupeloc.tag_id=fileduplicate.location ;
 
 
 -- Dumping structure for view aid.indexview
 -- Removing temporary table and create final VIEW structure
-DROP TABLE IF EXISTS `indexview`;
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` VIEW `indexview` AS SELECT a.id, CONCAT(dirlist.dirpath,filelist.filename) as fullpath, a.size, location_tags.location FROM `fileindex` as a JOIN filelist ON a.filename=filelist.id JOIN dirlist on a.dir=dirlist.id JOIN location_tags ON a.location = location_tags.tag_id ;
-SET FOREIGN_KEY_CHECKS=1;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+DROP TABLE IF EXISTS "indexview";
+CREATE OR REPLACE VIEW "indexview" AS 
+SELECT a.id, CONCAT(dirlist.dirpath,filelist.filename) as fullpath, a.size, location_tags.location FROM "fileindex" as a JOIN filelist ON a.filename=filelist.id JOIN dirlist on a.dir=dirlist.id JOIN location_tags ON a.location = location_tags.tag_id ;
+
